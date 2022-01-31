6d3564cdca769f79ecf1349a4c3465e257aa35c4
Bertan ULUSOY <bertan_ulusoy@yahoo.com>
2022-01-31 06:50:03 +0300

    Making OOP programming for  RandomForest Algorithm

--- algorithms/RandomForest.py
+++ algorithms/RandomForest.py
@@ -1,6 +1,29 @@
-from base.regressor import Regressor
+from egeai.algorithms.base.regressor import Regressor
+
+import pandas as Series
+import pandas as DataFrame
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
 
 
 class RandomForest(Regressor):
-    def __init__(self):
-        super.__init__(self)
+    def __init__(self, X_train: DataFrame, X_valid: DataFrame, y_train: Series, y_valid: Series):
+        print("sfsfsdfsdfs")
+        super().__init__(X_train, X_valid, y_train, y_valid)
+
+    def __repr__(self):
+        return 'RandomForest(*{!r})'.format(self.X_train)
+
+    def best_number_of_estimator(self) -> int:
+        return 10
+
+    def score(self):
+        n_estimators = self.best_number_of_estimator()
+        model = RandomForestRegressor(
+            n_estimators=n_estimators,
+            random_state=self.random_state
+        )
+        model.fit(self.X_train, self.y_train)
+        preds = model.predict(self.X_valid)
+        return mean_absolute_error(self.y_valid, preds)
--- algorithms/base/regressor.py
+++ algorithms/base/regressor.py
@@ -1,7 +1,18 @@
 import numpy as np
-import pandas as pd
+import pandas as Series
+import pandas as DataFrame
 
 
 class Regressor:
-    def __init__(self, X_train: pd.DataFrame, y_train: pd.Series) -> None:
+    def __init__(self, X_train: DataFrame, X_valid: DataFrame, y_train: Series, y_valid: Series) -> None:
         self.X_train = X_train
+        self.X_valid = X_valid
+        self.y_train = y_train
+        self.y_valid = y_valid
+        self.random_state = 42
+
+    def best_number_of_estimator(self) -> int:
+        pass
+
+    def score(self):
+        pass
